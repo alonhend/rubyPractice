cf11614546cd7b64b319cf6738587df43a200e09
Alon Hendelman <alon.hendelman@gmail.com>
2013-09-18 10:48:20 +0300

    First Initial Commit - Empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyPractice::Application.config.secret_key_base = '3aa04da481c8e95cac3304128bfe71bcf8770158e309e114a4002dd1a7a800d413dc3d31a246b2ff7956e09f98a3e00150b145887e969382b36ab01b42b00a79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyPractice::Application.config.session_store :cookie_store, key: '_rubyPractice_session'
